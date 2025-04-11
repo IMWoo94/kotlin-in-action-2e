a00b2cbbf2e1f05a7c08725063426eae53410213
sangmin.lee <ts1571@tidesquare.com>
2025-04-11 15:13:43 +0900

    feat : kotlin class unused lambda collections
    -  kotlin lambda by kotlin in action 2/e/ book

---
+++ kotlinInAction/src/main/kotlin/com/example/kotlinInAction/lambda/LambdaPerson.kt
@@ -0,0 +1,22 @@
+package com.example.kotlinInAction.lambda
+
+data class LambdaPerson(val name: String, val age: Int) {
+}
+
+fun findTheOldest(people: List<LambdaPerson>) {
+    var maxAge = 0
+    var theOldest: LambdaPerson? = null
+    for (person in people) {
+        if (person.age > maxAge) {
+            maxAge = person.age
+            theOldest = person
+        }
+    }
+
+    println(theOldest)
+}
+
+fun main() {
+    val people = listOf(LambdaPerson("Alice", 29), LambdaPerson("Bob", 31))
+    findTheOldest(people)
+}
